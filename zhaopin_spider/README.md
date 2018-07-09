bf2238fa38e49a3c8c0ecf8ee7afb99724d0ed83
”mengxianliang521” <mengxianliang@SH-mengxianliang.local>
2018-07-09 13:59:23 +0800

    dianshang_crawl

--- zhaopin_spider/README.md
+++
@@ -1,3 +0,0 @@
-# zhaopin_scrapy
-招聘网站分布式
-各大招聘网站分布式爬取,采用scrapy框架
--- zhaopin_spoder/zhaopin/run.py
+++
@@ -1,6 +0,0 @@
-from scrapy import cmdline
-
-import os
-os.chdir('zhaopin/spiders')
-
-cmdline.execute('scrapy crawl zhilian'.split())

310d295f4d972eacfa82484013341855145e0ce0
”mengxianliang521” <mengxianliang@SH-mengxianliang.local>
2018-07-09 13:43:56 +0800

    zhaopin_spider

--- Dianying_Flask0/app/home/__init__.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-
-#定义蓝图
-from flask import Blueprint
-
-home = Blueprint("home",__name__)
-
-import app.home.views
--- Dianying_Flask0/manage.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-from app import app
-
-if __name__ == "__main__":
-    app.run(
-        host='0.0.0.0',
-        port=7777,
-        debug=True
-    )
